22c5788737736a5bd6230567a701760becb6dee4
chip <49256163+chipanyanwu@users.noreply.github.com>
2023-09-23 04:05:15 -0500

    server code + data/route for restaurants endpoint

---
+++ server/data/restaurants.js
@@ -0,0 +1,9 @@
+const restaurantData = [
+  {
+    name: "Test",
+    description: "Desc",
+    imageUrl: "https://placehold.jp/500x500.png",
+  },
+]
+
+export default restaurantData
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express from "express"
 
-const app = express()
+import restaurantsRouter from "./routes/restaurants.js"
+
 const PORT = process.env.PORT || 3001
+const app = express()
 
 app.use("/public", express.static("./public"))
 app.use("/scripts", express.static("./public/scripts"))
+app.use("/restaurants", restaurantsRouter)
 
 app.get("/", (req, res) => {
   res
