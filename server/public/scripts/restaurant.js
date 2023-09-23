49148f3c083fa1dc0ce1e4ebb0fb46051487542d
chip <49256163+chipanyanwu@users.noreply.github.com>
2023-09-23 07:23:40 -0500

    finished detail page

---
+++ server/public/scripts/restaurant.js
@@ -0,0 +1,33 @@
+const renderRestaurant = async () => {
+  const requestedId = parseInt(window.location.href.split("/").pop())
+
+  const response = await fetch("/restaurants")
+  const data = await response.json()
+
+  const restaurantContent = document.getElementById("restaurant-content")
+
+  const restaurant = data.find((restaurant) => restaurant.id === requestedId)
+  if (restaurant) {
+    const image = document.getElementById("image")
+    const name = document.getElementById("name")
+    const address = document.getElementById("address")
+    const description = document.getElementById("description")
+
+    image.src = restaurant.imageUrl
+    name.textContent = restaurant.name
+    address.textContent = restaurant.address
+    description.textContent = restaurant.description
+
+    document.title = `CampusEats - ${restaurant.name}`
+
+    console.log("Here")
+  } else {
+    const message = document.createElement("h2")
+    message.textContent = "Could Not Find Restaurant 🍽️"
+
+    restaurantContent.appendChild(message)
+    console.log(data)
+  }
+}
+
+renderRestaurant()
--- server/public/scripts/restaurants.js
+++ server/public/scripts/restaurants.js
@@ -4,7 +4,57 @@ const renderRestaurants = async () => {
   const response = await fetch("/restaurants")
   const data = await response.json()
 
-  if (false) {
+  if (data) {
+    const cardContainer = document.createElement("div")
+    cardContainer.classList.add("container", "card-container")
+
+    data.map((restaurant) => {
+      const card = document.createElement("article")
+      card.classList.add("card")
+
+      // Image
+      const cardHeader = document.createElement("header")
+      cardHeader.classList.add("card-header")
+      cardHeader.style.backgroundImage = `url(${restaurant.imageUrl})`
+      cardHeader.style.backgroundSize = "cover"
+      cardHeader.style.backgroundRepeat = "no-repeat"
+      cardHeader.style.backgroundPosition = "center center"
+      cardHeader.style.height = "250px"
+
+      // Details (Name + Address + Description)
+      const cardTitles = document.createElement("hgroup")
+
+      const restaurantName = document.createElement("h2")
+      restaurantName.textContent = restaurant.name
+
+      const restaurantAddress = document.createElement("h3")
+      restaurantAddress.textContent = restaurant.address
+
+      const restaurantDescription = document.createElement("p")
+      restaurantDescription.textContent = restaurant.description
+
+      cardTitles.appendChild(restaurantName)
+      cardTitles.appendChild(restaurantAddress)
+
+      // Read More Button
+      const cardFooter = document.createElement("footer")
+
+      const link = document.createElement("a")
+      link.textContent = "Read More >"
+      link.setAttribute("role", "button")
+      link.href = `/restaurants/${restaurant.id}`
+
+      cardFooter.appendChild(link)
+
+      card.appendChild(cardHeader)
+      card.appendChild(cardTitles)
+      card.appendChild(restaurantDescription)
+      card.appendChild(cardFooter)
+
+      cardContainer.appendChild(card)
+    })
+
+    mainContent.appendChild(cardContainer)
   } else {
     const message = document.createElement("h2")
     message.textContent = "No Restaurants Found 🍽️"
